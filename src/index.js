479e1ea7169952a36e2e8d9d4bbaef622e7e5b90
Khursheed Ahmad <awslearning57@gmail.com>
2023-05-08 17:49:03 +0530

    Extracting App from index

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./pages/Layout"
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import App from './pages/App';
 import Table from './pages/Table';
 import Login from './pages/Login';
 import Nopage from './pages/Nopage';
 import Test from './pages/Test';
 
-const Apps = () => {
+const Index = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<App />} />
             <Route path='login' element={<Login />} />
             <Route path='table' element={<Table />} />
             <Route path='test' element={<Test />} />
@@ -27,5 +28,5 @@ const Apps = () => {
   );
 };
 
-ReactDOM.render(<Apps />, document.querySelector("#root"));
+ReactDOM.render(<Index />, document.querySelector("#root"));
 
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -16,7 +16,7 @@ const Layout = () => {
                         <div className="navbar-nav">
                             <ul className="nav">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                    <Link className="nav-link active" aria-current="page" to="/">App</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/login">Login</Link>
